312d581b5b619b71367f7409a6a64fb64788b789
Cedric KOCH <cedric.koch@outlook.com>
2017-12-14 19:04:37 +0100

    Revert "erreur"
    This reverts commit 49b968787614dc19b7d68364ae933d36324ddac4.

--- Coding/exo10/Form1.Designer.cs
+++ Coding/exo10/Form1.Designer.cs
@@ -57,7 +57,6 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "add";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -83,7 +82,6 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(165, 20);
             this.textBox1.TabIndex = 4;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // radioButton1
             // 
