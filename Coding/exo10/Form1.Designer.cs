c610a9da49d44e81a8a31829c5666129fe0b885b
S4kur5 <34523562+S4kur5@users.noreply.github.com>
2017-12-14 19:41:40 +0100

    exo10 fini

--- Coding/exo10/Form1.Designer.cs
+++ Coding/exo10/Form1.Designer.cs
@@ -67,6 +67,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "delete";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -76,6 +77,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "---->";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox1
             // 
@@ -139,6 +141,7 @@
             this.button4.TabIndex = 10;
             this.button4.Text = "add  -->";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
