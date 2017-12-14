49b968787614dc19b7d68364ae933d36324ddac4
S4kur5 <34523562+S4kur5@users.noreply.github.com>
2017-12-14 18:53:10 +0100

    erreur

--- Coding/exo10/Form1.cs
+++ Coding/exo10/Form1.cs
@@ -15,5 +15,15 @@ namespace exo10
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
