2adf7a6dacb0a0691ec72234eca03e0739481f59
t3ika <cedric.koch@outlook.com>
2017-12-14 15:02:25 +0100

    app1 template

--- Coding/app2/Form1.cs
+++ Coding/app2/Form1.cs
@@ -6,25 +6,14 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using System.Configuration;
 
-namespace app1
+namespace app2
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
