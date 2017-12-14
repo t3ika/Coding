de5d74594869ceb69674eed8a0bca177db125317
S4kur5 <34523562+S4kur5@users.noreply.github.com>
2017-12-14 23:45:11 +0100

    90% app1

--- Coding/app1/Form1.Designer.cs
+++ Coding/app1/Form1.Designer.cs
@@ -43,11 +43,14 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -72,20 +75,21 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(33, 79);
+            this.button2.Location = new System.Drawing.Point(33, 108);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "-->";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(33, 108);
+            this.button3.Location = new System.Drawing.Point(33, 137);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
+            this.button3.Text = "<----";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
@@ -94,8 +98,9 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
+            this.button4.Text = "--->";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -103,21 +108,21 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 5;
-            this.button5.Text = "button5";
+            this.button5.Text = "<----";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(6, 19);
+            this.listBox1.Location = new System.Drawing.Point(6, 45);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(391, 537);
+            this.listBox1.Size = new System.Drawing.Size(391, 511);
             this.listBox1.TabIndex = 6;
             // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(145, 19);
+            this.listBox2.Location = new System.Drawing.Point(145, 41);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(237, 199);
             this.listBox2.TabIndex = 7;
@@ -149,27 +154,27 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(6, 75);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(27, 13);
             this.label1.TabIndex = 11;
-            this.label1.Text = "label1";
+            this.label1.Text = "nom";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(6, 129);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(42, 13);
             this.label2.TabIndex = 12;
-            this.label2.Text = "label2";
+            this.label2.Text = "prenom";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(6, 181);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(78, 13);
             this.label3.TabIndex = 13;
-            this.label3.Text = "label3";
+            this.label3.Text = "niveau (1 a 10)";
             // 
             // groupBox1
             // 
@@ -191,29 +196,41 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "inscription";
             // 
-            // checkBox1
+            // comboBox1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(54, 257);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 14;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Mr",
+            "Mme"});
+            this.comboBox1.Location = new System.Drawing.Point(9, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 16;
             // 
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
             this.checkBox2.Location = new System.Drawing.Point(54, 281);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.Size = new System.Drawing.Size(58, 17);
             this.checkBox2.TabIndex = 15;
-            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.Text = "double";
             this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(54, 257);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(55, 17);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "simple";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // groupBox2
             // 
             this.groupBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox2.Controls.Add(this.button6);
             this.groupBox2.Controls.Add(this.listBox1);
             this.groupBox2.Location = new System.Drawing.Point(251, 12);
             this.groupBox2.Name = "groupBox2";
@@ -224,6 +241,8 @@
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.label4);
             this.groupBox3.Controls.Add(this.listBox2);
             this.groupBox3.Controls.Add(this.listBox3);
             this.groupBox3.Controls.Add(this.button5);
@@ -237,16 +256,33 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "groupBox3";
             // 
-            // comboBox1
+            // button6
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Mr",
-            "Mme"});
-            this.comboBox1.Location = new System.Drawing.Point(9, 41);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 16;
+            this.button6.Location = new System.Drawing.Point(239, 16);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(158, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "charger";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(145, 323);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "double";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(145, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "simple";
             // 
             // Form1
             // 
@@ -263,6 +299,7 @@
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -289,6 +326,9 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Coding/app1/Form1.cs
+++ Coding/app1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Configuration;
+using System.IO;
 
 namespace app1
 {
@@ -16,15 +17,83 @@ namespace app1
         {
             InitializeComponent();
         }
+        //static async void WriteTextAsync(string text)
+        //{
+        //    // Set a variable to the My Documents path.
+        //    string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
+        //    // Write the text asynchronously to a new file named "WriteTextAsync.txt".
+        //    using (StreamWriter outputFile = new StreamWriter(mydocpath + @"\WriteTextAsync.txt"))
+        //    {
+        //        await outputFile.WriteAsync(text);
+        //    }
+        //}
         private void button1_Click(object sender, EventArgs e)
         {
-            //*
+            if (checkBox2.Checked == true && checkBox1.Checked == true)
+            {
+                    var p = "simple";
+                    var l = "double";
+                System.IO.File.AppendAllText(@"C:/lol/Test.txt", Environment.NewLine + comboBox1.SelectedItem + " " + textBox1.Text + " " + textBox2.Text + " " + textBox3.Text + " " + p + " " + l);
+                return;
+
+            }
+            if (checkBox1.Checked == true)
+            {
+
+                    var a = "simple";
+                System.IO.File.AppendAllText(@"C:/lol/Test.txt", Environment.NewLine + comboBox1.SelectedItem + " " + textBox1.Text + " " + textBox2.Text + " " + textBox3.Text + " " + a);
+
+            }
+          else if (checkBox2.Checked == true)
+            {
+                var b = "double";
+                System.IO.File.AppendAllText(@"C:/lol/Test.txt", Environment.NewLine+ comboBox1.SelectedItem + " " + textBox1.Text + " " + textBox2.Text + " " + textBox3.Text+" " +b);
+            }
+
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listBox1.DataSource = File.ReadAllLines("C:/lol/Test.txt");
+            object[] varObjet = new object[listBox1.Items.Count];
+            listBox1.Items.CopyTo(varObjet, 0);
+            listBox1.DataSource = null;
+            listBox1.Items.AddRange(varObjet);
+
+            //StreamReader sr = null;
+            //sr = new StreamReader(@"C:/lol/Test.txt");
+            //string line = sr.ReadLine();
+            //while (line != null)
+            //{
+            //    this.listBox1.Items.Add(line);
+            //    line = sr.ReadLine();
+            //}
+            //Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(listBox1.SelectedItem);
+            listBox1.Items.Remove(listBox1.SelectedItem);
+            listBox1.ClearSelected();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(listBox1.SelectedItem);
+            listBox1.Items.Remove(listBox1.SelectedItem);
+            listBox1.ClearSelected();
         }
     }
 }
