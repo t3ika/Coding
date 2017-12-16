9f3a108a830b65445932f38e2740f26d1579974f
Cedric KOCH <cedric.koch@outlook.com>
2017-12-16 01:06:15 +0100

    app3

--- Coding/app3/Form1.cs
+++ Coding/app3/Form1.cs
@@ -5,20 +5,16 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace app2
+namespace app3
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
